f975af318ece31d8b31a91ec2e8a472f376e83b0
samuel Edelstein <sammy@samuels-MacBook-Pro.local>
2021-11-07 16:28:32 -0600

    save output to tempfile

--- models/util_model.py
+++ models/util_model.py
@@ -9,6 +9,7 @@ def save_sklearn_model(model, filename):
     fd, path = tempfile.mkstemp()
     joblib.dump(model, path)
     requests.put(filename, open(path, "rb"))
+    logger.info(f"** Successfully saved {model} in {path}")
     logger.info(f"** Successfully saved {model} in {filename}")
 
 def save_clustering_output(node_id_list, cluster_id_list, filename):
@@ -17,11 +18,17 @@ def save_clustering_output(node_id_list, cluster_id_list, filename):
     fd, path = tempfile.mkstemp()
     output_df.to_csv(path)
     requests.put(filename, open(path, "rb"))
+    logger.info(f"** Successfully saved clustering output in {path}")
     logger.info(f"** Successfully saved clustering output in {filename}")
 
 def save_model_performance(perf_dict, filename):
     json_obj = json.dumps(perf_dict)
+    fd, path = tempfile.mkstemp()
     requests.put(filename, json_obj)
+    file = open(path, "a")
+    file.write(json_obj)
+    file.close()
+    logger.info(f"** Successfully saved performance dictionary in {path}")
     logger.info(f"** Successfully saved performance dictionary in {filename}")
 
 
