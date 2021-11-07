ca84c51ac966f640812ffe289713271587320147
samuel Edelstein <sammy@samuels-MacBook-Pro.local>
2021-11-07 09:23:51 -0600

    use requests pkg for uploading files

--- models/util_model.py
+++ models/util_model.py
@@ -1,21 +1,28 @@
 import logging, joblib, json, pickle
 import pandas as pd
+import requests
+import pdb
+import tempfile
 
 logger = logging.getLogger()
 
 def save_sklearn_model(model, filename):
-    joblib.dump(model, filename)
+    fd, path = tempfile.mkstemp()
+    joblib.dump(model, path)
+    requests.put(filename, open(path, "rb"))
     logger.info(f"** Successfully saved {model} in {filename}")
 
 def save_clustering_output(node_id_list, cluster_id_list, filename):
     output_df = pd.DataFrame([node_id_list, cluster_id_list]).transpose()
     output_df.columns = ['node_id', 'cluster_id']
-    output_df.to_csv(filename)
+    fd, path = tempfile.mkstemp()
+    output_df.to_csv(path)
+    requests.put(filename, open(path, "rb"))
     logger.info(f"** Successfully saved clustering output in {filename}")
 
 def save_model_performance(perf_dict, filename):
-    with open(filename, 'w') as fh:
-        json.dump(perf_dict, fh)
+    json_obj = json.dumps(perf_dict)
+    requests.put(filename, json_obj)
     logger.info(f"** Successfully saved performance dictionary in {filename}")
 
 
